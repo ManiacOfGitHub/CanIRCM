411ba9a8a6a85b417ce33275f4c64b3a69ca2128
ManiacOfGitHub <43019219+ManiacOfGitHub@users.noreply.github.com>
2026-08-24 21:12:12 -0400

    goofy serials

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("form").addEventListener("submit",(event)=>{
     var serial = document.getElementById("serial").value.toUpperCase().replaceAll(" ","");
 
     var result = checkSerial(serial);
+    if(!result) return;
     var outputs = {
         mariko: `seems to be a "mariko" Switch, Switch Lite, or Switch OLED.\nThese are currently not hackable via software, only hardware modifications that involve soldering modchips.<br><br>You can find a list of hardmodders trusted by the community <a href="https://nintendohomebrew.com/hardmodders">here</a>. When requesting their services, please make sure to specify your console model (Switch v2, Switch OLED, Switch Lite) after reviewing the diagram below:`,
         switch2: `seems to be a Switch 2. These are currently not hackable.`,
@@ -27,7 +28,11 @@ document.getElementById("form").addEventListener("submit",(event)=>{
 
 function checkSerial(serial) {
     document.getElementById("modelTable").style.display = "none";
-    if(serial.length != 14) return "invalid";
+    if(serial.length != 14) {
+        var stupidCheck = checkSerialButReallyStupid(serial);
+        if(stupidCheck) return;
+        return "invalid";
+    }
     if(!serial.match("XA[JKW][1479][0-9]{6}")) {
         if(serial.match("X[KJWT][JWCE][0-9]{7}")) {
             document.getElementById("modelTable").style.display = "block";
@@ -35,6 +40,8 @@ function checkSerial(serial) {
         } else if(serial.match("HA[JKWE][0-9]{7}")) {
             return "switch2";
         } else {
+            var stupidCheck = checkSerialButReallyStupid(serial);
+            if(stupidCheck) return;
             return "invalid";
         }
     }
@@ -64,4 +71,47 @@ function checkSerial(serial) {
     } else {
         return "patched";
     }
+}
+
+
+// Nothing under this line matters, it's all for lols.
+
+function checkSerialButReallyStupid(serial) {
+    // 3DS goes first before the other nonsense.
+    if(serial.match("[CESRAPYQN](?:JE|JF|JH|JM|W|B|S|EE|EF|EH|EM|AF|AH|AG|AM|KF|KH|KM|CF|CH|CM)[0-9]{9}")) {
+        document.getElementById("serialOutput").innerText = serial;
+        document.getElementById("outputMessage").innerHTML = `is a console within the Nintendo 3DS family of systems. Did you know it's surprisingly easy to mod?<br><br><p class="text-center"><a href="https://3ds.hacks.guide"><img style="max-width: 100%;" src="img/3ds.jpeg"></a></p>`;
+        document.getElementById("output").style.display = "block";
+        document.getElementById("outputAlert").className = "alert alert-primary";
+        return true;
+    }
+
+    if(serial.match("X[A-Z][JWCEL][0-9]{7}") && serial.length == 14) {
+        var products = {
+            "B": "Left Joy-Con",
+            "C": "Right Joy-Con",
+            "D": "Joy-Con charge grip",
+            "E": "Pro Controller",
+            "F": "Dock",
+            "H": "Pok\u00e9 Ball Plus controller",
+            "L": "Left NES controller",
+            "M": "Right NES controller",
+            "N": "Left Famicom controller",
+            "P": "Right Famicom controller",
+            "Q": "Kart for \"Mario Kart Live: Home Circuit\"",
+            "R": "SNES controller",
+            "S": "N64 controller",
+            "X": "Sega Genesis controller",
+            "Y": "RelayBox",
+            "Z": "SDEV cradle"
+        };
+        if(products.hasOwnProperty(serial[1])) {
+            document.getElementById("serialOutput").innerText = serial;
+            document.getElementById("outputMessage").innerHTML = `is a ${products[serial[1]]}. I would venture to guess that it is not vulnerable to fusee-gelee. Just a hunch.`
+            document.getElementById("output").style.display = "block";
+            document.getElementById("outputAlert").className = "alert alert-secondary";
+            return true;
+        }
+    }
+    return false;
 }
