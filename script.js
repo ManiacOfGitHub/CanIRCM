f7f16b869df527865d273c1bd04d594c3d1966e9
ManiacOfGitHub <43019219+ManiacOfGitHub@users.noreply.github.com>
2026-08-24 20:01:07 -0400

    add console images, update messages and serial number ranges closes #12 This also adds messages for "potentially patched" to tell the user what to do it if it ends up being patched along with what information to give the hardmodder (basically how to find the model) Also, update the serial ranges based on the PR opened within the switch guide.

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.getElementById("form").addEventListener("submit",(event)=>{
 
     var result = checkSerial(serial);
     var outputs = {
-        mariko: `seems to be a "mariko" Switch or Switch Lite.\nThese are currently not hackable via software, only hardware modifications that involve soldering modchips. You can find a list of hardmodders trusted by the community <a href="https://nintendohomebrew.com/hardmodders">here</a>.`,
+        mariko: `seems to be a "mariko" Switch, Switch Lite, or Switch OLED.\nThese are currently not hackable via software, only hardware modifications that involve soldering modchips.<br><br>You can find a list of hardmodders trusted by the community <a href="https://nintendohomebrew.com/hardmodders">here</a>. When requesting their services, please make sure to specify your console model (Switch v2, Switch OLED, Switch Lite) after reviewing the diagram below:`,
         switch2: `seems to be a Switch 2. These are currently not hackable.`,
-        maybe: `<i>might</i> be patched. The only way you can know this for sure is by pushing the payload manually. You can find instructions to do so <a href="https://switch.hacks.guide/user_guide/rcm/sending_payload.html">here</a>.`,
-        patched: `is patched. It is not hackable via software, only hardware modifications that involve soldering modchips. You can find a list of hardmodders trusted by the community <a href="https://nintendohomebrew.com/hardmodders">here</a>.`,
+        maybe: `<i>might</i> be patched. The only way you can know this for sure is by pushing the payload manually. You can find instructions to do so <a href="https://switch.hacks.guide/user_guide/rcm/sending_payload.html">here</a>.<br>If you find out the console is patched, then that means the console is not hackable with software, only hardware modifications that involve soldering modchips.<br><br>You can find a list of hardmodders trusted by the community <a href="https://nintendohomebrew.com/hardmodders">here</a>. When requesting their services, please make sure to specify that your console model is a patched Erista (v1) Switch.`,
+        patched: `is patched. It is not hackable via software, only hardware modifications that involve soldering modchips.<br><br>You can find a list of hardmodders trusted by the community <a href="https://nintendohomebrew.com/hardmodders">here</a>. When requesting their services, please make sure to specify that your console model is a patched Erista (v1) Switch.`,
         unpatched: `is not patched. <a href="https://switch.hacks.guide/user_guide/rcm/sending_payload.html">Continue to the written guide</a>.`,
         invalid: `is invalid. Please make sure you typed it correctly.`
     };
@@ -26,9 +26,11 @@ document.getElementById("form").addEventListener("submit",(event)=>{
 })
 
 function checkSerial(serial) {
+    document.getElementById("modelTable").style.display = "none";
     if(serial.length != 14) return "invalid";
     if(!serial.match("XA[JKW][1479][0-9]{6}")) {
         if(serial.match("X[KJWT][JWCE][0-9]{7}")) {
+            document.getElementById("modelTable").style.display = "block";
             return "mariko";
         } else if(serial.match("HA[JKWE][0-9]{7}")) {
             return "switch2";
@@ -44,12 +46,12 @@ function checkSerial(serial) {
     var maxSafe = {
         "W": {
             1: {unpatched: 1006500, maybe: 1012000},
-            4: {unpatched: 4001100, maybe: 4001200},
-            7: {unpatched: 7001780, maybe: 7003000}
+            4: {unpatched: 4001100, maybe: 4001300},
+            7: {unpatched: 7001750, maybe: 7003000}
         },
         "J": {
             1: {unpatched: 1002000, maybe: 1003000},
-            4: {unpatched: 4004400, maybe: 4006000},
+            4: {unpatched: 4004400, maybe: 4008300},
             7: {unpatched: 7004000, maybe: 7005000}
         }
     }
